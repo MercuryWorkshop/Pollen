c3a0887af2c6a1d1c2fba614ae62e90a79aea5f9
Scarat3k <companioncubegd@gmail.com>
2023-02-04 15:13:17 -0500

    Create RootFS.sh

---
+++ Scripts/RootFS.sh
@@ -0,0 +1,2 @@
+/usr/share/vboot/bin/make_dev_ssd.sh -i /dev/mmcblk0 --remove_rootfs_verification --partitions 2
+/usr/share/vboot/bin/make_dev_ssd.sh -i /dev/mmcblk0 --remove_rootfs_verification --partitions 4
