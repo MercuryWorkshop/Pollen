770400d0782e274c6da1d84cedaec420f846482d
Scarat3k <companioncubegd@gmail.com>
2023-02-05 00:50:10 -0500

    Delete Scripts directory

--- Scripts/Original Scripts/RootFS.sh
+++
@@ -1,2 +0,0 @@
-/usr/share/vboot/bin/make_dev_ssd.sh -i /dev/mmcblk0 --remove_rootfs_verification --partitions 2
-/usr/share/vboot/bin/make_dev_ssd.sh -i /dev/mmcblk0 --remove_rootfs_verification --partitions 4
