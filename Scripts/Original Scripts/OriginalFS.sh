26d0312738c4afd22033220710b3428cf4488309
Scarat3k <companioncubegd@gmail.com>
2023-02-04 20:06:33 -0500

    Delete Scripts directory

--- Scripts/Original Scripts/OriginalFS.sh
+++
@@ -1,2 +0,0 @@
-mkdir -p /etc/opt/chrome/policies/managed
-echo '{"URLBlocklist": [], "SystemFeaturesDisableList": [], "ChromeOsMultiProfileUserBehavior": "unrestricted", "DeveloperToolsAvailability": 1, "AllowDeletingBrowserHistory": true, "IncognitoModeAvailability": 0, "AllowScreenLock": true}' > /etc/opt/chrome/policies/managed/policy.json
