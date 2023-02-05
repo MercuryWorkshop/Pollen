6b02ac01513e4874f758b3638d11ed28938f08c3
Scarat3k <companioncubegd@gmail.com>
2023-02-05 01:25:14 -0500

    Update Pollen.sh

--- Pollen.sh
+++ Pollen.sh
@@ -100,6 +100,5 @@ sudo mkdir -p /tmp/overlay/etc/opt/chrome/policies/managed
 sudo echo $pollen > /tmp/overlay/etc/opt/chrome/policies/managed/policy.json
 sudo cp -a -L /etc/* /tmp/overlay/etc 2> /dev/null
 sudo mount --bind /tmp/overlay/etc /etc
-clear
 
 echo "Pollen Has Been Successfully Applied!"
