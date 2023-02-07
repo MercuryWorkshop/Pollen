a77f9d2eeb8d340b0702ead97d9f72c05aa98f75
Scarat3k <companioncubegd@gmail.com>
2023-02-06 20:39:25 -0500

    Delete RootFS.sh

--- RootFS.sh
+++
@@ -1,3 +0,0 @@
-sudo /usr/share/vboot/bin/make_dev_ssd.sh -i /dev/mmcblk0 --remove_rootfs_verification --partitions 2
-sudo /usr/share/vboot/bin/make_dev_ssd.sh -i /dev/mmcblk0 --remove_rootfs_verification --partitions 4
-echo "RootFS Has Been Disabled!"
