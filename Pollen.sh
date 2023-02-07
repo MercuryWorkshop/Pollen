5ae289533c0973c5dbc106affe6611ddfdd180a2
Scarat3k <companioncubegd@gmail.com>
2023-02-06 20:44:45 -0500

    Update Pollen.sh

--- Pollen.sh
+++ Pollen.sh
@@ -12,9 +12,9 @@ echo "Made The Original Script - OlyB"
 echo "###############################"
 sleep 5
 
-sudo mkdir -p /tmp/overlay/etc/opt/chrome/policies/managed
-sudo echo '{"URLBlocklist": [], "SystemFeaturesDisableList": [], "ChromeOsMultiProfileUserBehavior": "unrestricted", "DeveloperToolsAvailability": 1, "AllowDeletingBrowserHistory": true, "IncognitoModeAvailability": 0, "AllowScreenLock": true, "ExtensionAllowedTypes": null, "ExtensionInstallAllowlist": null, "ExtensionInstallBlocklist": null, "ExtensionInstallForcelist": null, "ExtensionSettings": null}' > /tmp/overlay/etc/opt/chrome/policies/managed/policy.json
-sudo cp -a -L /etc/* /tmp/overlay/etc 2> /dev/null
-sudo mount --bind /tmp/overlay/etc /etc
+mkdir -p /tmp/overlay/etc/opt/chrome/policies/managed
+echo '{"URLBlocklist": [], "SystemFeaturesDisableList": [], "ChromeOsMultiProfileUserBehavior": "unrestricted", "DeveloperToolsAvailability": 1, "AllowDeletingBrowserHistory": true, "IncognitoModeAvailability": 0, "AllowScreenLock": true, "ExtensionAllowedTypes": null, "ExtensionInstallAllowlist": null, "ExtensionInstallBlocklist": null, "ExtensionInstallForcelist": null, "ExtensionSettings": null}' > /tmp/overlay/etc/opt/chrome/policies/managed/policy.json
+cp -a -L /etc/* /tmp/overlay/etc 2> /dev/null
+mount --bind /tmp/overlay/etc /etc
 echo "Pollen Has Been Successfully Applied!"
 
