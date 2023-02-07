e5832ac327b77d8f340439a274896c89bd4b298c
r58Playz <45221816+r58Playz@users.noreply.github.com>
2023-02-06 17:54:52 -0800

    tiny bug

--- Pollen.sh
+++ Pollen.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 clear
-echo "###############################"
-echo "Welcome To Pollen!"
-echo "The User Policy Editor"
-echo "----------------------"
-echo "Credit:"
-echo "Failed And Failed Again (Useless) - Scaratek"
-echo "Logo Designer - Nitelite"
-echo "Found The Overlook - Rafflesia"
-echo "Made The Original Script - OlyB"
-echo "###############################"
-sleep 5
+echo "+##############################################+"
+echo "# Welcome To Pollen (v1)!                      #"
+echo "# The User Policy Editor                       #"
+echo "# -------------------------------------------- #"
+echo "# Credit:                                      #"
+echo "# Failed And Failed Again (Useless) - Scaratek #"
+echo "# Logo Designer - Nitelite                     #"
+echo "# Found The Overlook - Rafflesia               #"
+echo "# Made The Original Script - OlyB              #"
+echo "# Fixed a tiny little bug - r58Playz           #"
+echo "+##############################################+"
+sleep 2
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
 
 mkdir -p /tmp/overlay/etc/opt/chrome/policies/managed
-sudo echo '{"URLBlocklist": [], "SystemFeaturesDisableList": [], "ChromeOsMultiProfileUserBehavior": "unrestricted", "DeveloperToolsAvailability": 1, "AllowDeletingBrowserHistory": true, "IncognitoModeAvailability": 0, "AllowScreenLock": true, "ExtensionAllowedTypes": null, "ExtensionInstallAllowlist": null, "ExtensionInstallBlocklist": null, "ExtensionInstallForcelist": null, "ExtensionSettings": null}' > /tmp/overlay/etc/opt/chrome/policies/managed/policy.json
+echo '{"URLBlocklist": [], "SystemFeaturesDisableList": [], "ChromeOsMultiProfileUserBehavior": "unrestricted", "DeveloperToolsAvailability": 1, "AllowDeletingBrowserHistory": true, "IncognitoModeAvailability": 0, "AllowScreenLock": true, "ExtensionAllowedTypes": null, "ExtensionInstallAllowlist": null, "ExtensionInstallBlocklist": null, "ExtensionInstallForcelist": null, "ExtensionSettings": null}' > /tmp/overlay/etc/opt/chrome/policies/managed/policy.json
 cp -a -L /etc/* /tmp/overlay/etc 2> /dev/null
 sudo mount --bind /tmp/overlay/etc /etc
 echo "Pollen Has Been Successfully Applied!"
