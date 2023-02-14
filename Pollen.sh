51691309377e6fc06c87a1f2f5979f648c27a79f
BinBashBanana <51469593+BinBashBanana@users.noreply.github.com>
2023-02-13 23:59:06 -0800

    updates

--- Pollen.sh
+++ Pollen.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-clear
+
+if [ $(id -u) -ne 0 ]; then
+    echo "Run this script as root (sudo)"
+    exit
+fi
+
 echo "+##############################################+"
 echo "# Welcome To Pollen (v1)!                      #"
 echo "# The User Policy Editor                       #"
@@ -7,19 +12,15 @@ echo "# -------------------------------------------- #"
 echo "# Credit:                                      #"
 echo "# Pollen Developer - Scaratek                  #"
 echo "# Logo Designer - Nitelite                     #"
-echo "# Found The Overlook - Rafflesia               #"
-echo "# Made The Original Script - OlyB              #"
+echo "# Found the Overlook - Rafflesia               #"
+echo "# Made the Original Script - OlyB              #"
 echo "# Fixed a tiny little bug - r58Playz           #"
 echo "+##############################################+"
-sleep 2
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
 
 mkdir -p /tmp/overlay/etc/opt/chrome/policies/managed
 echo '{"URLBlocklist": [], "SystemFeaturesDisableList": [], "ChromeOsMultiProfileUserBehavior": "unrestricted", "DeveloperToolsAvailability": 1, "AllowDeletingBrowserHistory": true, "IncognitoModeAvailability": 0, "AllowScreenLock": true, "ExtensionAllowedTypes": null, "ExtensionInstallAllowlist": null, "ExtensionInstallBlocklist": null, "ExtensionInstallForcelist": null, "ExtensionSettings": null}' > /tmp/overlay/etc/opt/chrome/policies/managed/policy.json
 cp -a -L /etc/* /tmp/overlay/etc 2> /dev/null
-sudo mount --bind /tmp/overlay/etc /etc
-echo "Pollen Has Been Successfully Applied!"
+mount --bind /tmp/overlay/etc /etc
 
+echo ""
+echo "Pollen has been successfully applied!"
