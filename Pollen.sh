aacbbd6144a2c863637fca9a8b475291061d8bbe
5less-chromosones <126161241+5less-chromosones@users.noreply.github.com>
2023-02-28 11:28:57 +0000

    Update Pollen.sh

--- Pollen.sh
+++ Pollen.sh
@@ -15,11 +15,12 @@ echo "# Logo Designer - Nitelite                     #"
 echo "# Found the Overlook - Rafflesia               #"
 echo "# Made the Original Script - OlyB              #"
 echo "# Fixed a tiny little bug - r58Playz           #"
+echo "# Edited more policies - 5less-chromosones     #"
 echo "+##############################################+"
 echo "May Ultrablue Rest In Peace o7"
 
 mkdir -p /tmp/overlay/etc/opt/chrome/policies/managed
-echo '{"URLBlocklist": [], "SystemFeaturesDisableList": [], "WallpaperImage": {"hash": "26849a13ee7e81011e5b9a230a4a1a6ce5e92b232f388bccc1ca8a1b4533062d", "url": "https://m.media-amazon.com/images/M/MV5BZmU5ZDE5NTItN2I1YS00ZmFmLTk3YTgtNzQwOGNkYzFjOWRkXkEyXkFqcGdeQXVyNzU1NzE3NTg@._V1_QL75_UX500_CR0,47,500,281_.jpg"}, "ChromeOsMultiProfileUserBehavior": "unrestricted", "DeveloperToolsAvailability": 1, "DefaultPopupsSetting": 1, "AllowDeletingBrowserHistory": true, "AllowDinosaurEasterEgg": true, "IncognitoModeAvailability": 0, "AllowScreenLock": true, "ExtensionAllowedTypes": null, "ExtensionInstallAllowlist": null, "ExtensionInstallBlocklist": null, "ExtensionInstallForcelist": null, "ExtensionSettings": null}' > /tmp/overlay/etc/opt/chrome/policies/managed/policy.json
+echo '{"URLBlocklist": [], "SystemFeaturesDisableList": [], "ChromeOsMultiProfileUserBehavior": "unrestricted", "DeveloperToolsAvailability": 1, "DefaultPopupsSetting": 1, "AllowDeletingBrowserHistory": true, "AllowDinosaurEasterEgg": true, "IncognitoModeAvailability": 0, "AllowScreenLock": true, "ExtensionAllowedTypes": null, "ExtensionInstallAllowlist": null, "ExtensionInstallBlocklist": null, "ExtensionInstallForcelist": null, "ExtensionSettings": null}' > /tmp/overlay/etc/opt/chrome/policies/managed/policy.json
 cp -a -L /etc/* /tmp/overlay/etc 2> /dev/null
 mount --bind /tmp/overlay/etc /etc
 
