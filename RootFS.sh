48ad593cf156c3a23d0157957e330509d7d0c6df
CompanionCubeGD <companioncubegd@gmail.com>
2023-06-27 22:34:56 -0400

    PollenFS

---
+++ RootFS.sh
@@ -0,0 +1,3 @@
+sudo /usr/share/vboot/bin/make_dev_ssd.sh -i /dev/mmcblk0 --remove_rootfs_verification --partitions 2
+sudo /usr/share/vboot/bin/make_dev_ssd.sh -i /dev/mmcblk0 --remove_rootfs_verification --partitions 4
+echo "RootFS Has Been Disabled!"
