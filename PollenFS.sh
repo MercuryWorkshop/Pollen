b005a4943e098fcd91e84d8835335f6690fd9722
Entrpix <korr626@outlook.com>
2024-04-30 17:12:02 -0400

    readme is good now??

--- PollenFS.sh
+++ PollenFS.sh
@@ -6,13 +6,12 @@ if [ $(id -u) -ne 0 ]; then
 fi
 
 echo "+##############################################+"
-echo "# Welcome To PollenFS!                         #"
+echo "# Welcome To Pollen! (RootFS)                   #"
 echo "# The User Policy Editor                       #"
 echo "# -------------------------------------------- #"
 echo "# Developers:                                  #"
 echo "# - OlyB                                       #"
 echo "# - Rafflesia                                  #"
-echo "# - Scaratek                                   #"
 echo "# - r58Playz                                   #"
 echo "+##############################################+"
 echo "May Ultrablue Rest in Peace, o7"
@@ -20,27 +19,73 @@ echo "May Ultrablue Rest in Peace, o7"
 sleep 1
 
 mkdir -p /etc/opt/chrome/policies/managed
-printf '
-{
-    "URLBlocklist": [], 
-    "SystemFeaturesDisableList": [], 
-    "EditBookmarksEnabled": true, 
-    "ChromeOsMultiProfileUserBehavior": "unrestricted", 
-    "DeveloperToolsAvailability": 1, 
-    "DefaultPopupsSetting": 1, 
-    "AllowDeletingBrowserHistory": true, 
-    "AllowDinosaurEasterEgg": true, 
-    "IncognitoModeAvailability": 0, 
-    "AllowScreenLock": true, 
-    "ExtensionAllowedTypes": null, 
-    "ExtensionInstallAllowlist": null, 
-    "ExtensionInstallBlocklist": null, 
-    "ExtensionInstallForcelist": null, 
-    "ExtensionSettings": null, 
-    "LacrosAvailability": "user_choice", 
-    "WallpaperImage": null
-}
-' > /etc/opt/chrome/policies/managed/pollen.json
+echo '{
+  "URLBlocklist": [],
+  "SystemFeaturesDisableList": [],
+  "EditBookmarksEnabled": true,
+  "ChromeOsMultiProfileUserBehavior": "unrestricted",
+  "DeveloperToolsAvailability": 1,
+  "DefaultPopupsSetting": 1,
+  "AllowDeletingBrowserHistory": true,
+  "AllowDinosaurEasterEgg": true,
+  "IncognitoModeAvailability": 0,
+  "AllowScreenLock": true,
+  "ExtensionAllowedTypes": null,
+  "ExtensionInstallAllowlist": null,
+  "ExtensionInstallBlocklist": null,
+  "ExtensionInstallForcelist": null,
+  "ExtensionSettings": null,
+  "PasswordManagerEnabled": "true",
+  "TaskManagerEndProcessEnabled": "true",
+  "UptimeLimit": "null",
+  "SystemTerminalSshAllowed": "true",
+  "SystemTimezone": "",
+  "IsolatedAppsDeveloperModeAllowed": "true",
+  "ForceGoogleSafeSearch": "false",
+  "ForceYouTubeRestrict": "0",
+  "EasyUnlockAllowed": "true",
+  "DisableSafeBrowsingProceedAnyway": "false",
+  "DeviceAllowNewUsers": "true",
+  "DevicePowerAdaptiveChargingEnabled": "true",
+  "DeviceGuestModeEnabled": "true",
+  "DeviceUnaffiliatedCrostiniAllowed": "true",
+  "VirtualMachinesAllowed": "true",
+  "CrostiniAllowed": "true",
+  "DefaultCookiesSetting": "1",
+  "VmManagementCliAllowed": "true",
+  "WifiSyncAndroidAllowed": "true",
+  "DeveloperToolsDisabled": "false",
+  "DeveloperToolsAvailability": "1",
+  "DeviceBlockDevmode": "false",
+  "UserBorealisAllowed": "true",
+  "InstantTetheringAllowed": "true",
+  "NearbyShareAllowed": "true",
+  "PinnedLauncherApps": "null",
+  "PrintingEnabled": "true",
+  "SmartLockSigninAllowed": "true",
+  "PhoneHubAllowed": "true",
+  "LacrosAvailability": "user_choice",
+  "WallpaperImage": null,
+  "ArcPolicy": {
+    "playStoreMode": "ENABLED",
+    "installType": "FORCE_INSTALLED",
+    "playEmmApiInstallDisabled": false,
+    "dpsInteractionsDisabled": false
+  },
+  "DnsOverHttpsMode": "automatic",
+  "BrowserLabsEnabled": "true",
+  "ChromeOsReleaseChannelDelegated": "true",
+  "WallpaperImage": "null",
+  "SafeSitesFilterBehavior": "0",
+  "SafeBrowsingProtectionLevel": "0",
+  "DownloadRestrictions": "0",
+  "ProxyMode": "system",
+  "ProxyServerMode": "system",
+  "NetworkThrottlingEnabled": "false",
+  "NetworkPredictionOptions": "0",
+  "AllowedDomainsForApps": "",
+  "DeviceUserAllowlist": ""
+}' > /etc/opt/chrome/policies/managed/pollen.json
 
 echo ""
-echo "PollenFS has been successfully applied!"
+echo "Pollen (RootFS) has been successfully applied!"
